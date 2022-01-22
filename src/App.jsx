dc80324234ba39b61e8ddc58aa1801fe56b8426d
Gabriel Caramés <49535753+GabrielCarames@users.noreply.github.com>
2022-01-22 14:17:56 -0300

    Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
 
 function App() {
 
   return (
     <Router>
         <div className="app-container">
+            <Navbar />
+            <Sidebar />
             <Switch>
                 <Route exact path="/"><Home/></Route>
                 {/* <Route exact path="/error/:error"><Error/></Route> */}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,316 @@
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
-
 export default function Home() {
   return (
-      <div className="home">
-          <Navbar />
-          <Sidebar />
-      </div>
+    <div className="home">
+        <ul className="list">
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        Ibai reacciona a Zilverk por primera vez de DonIbair
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name">DonIbair</span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations">34.100 visualizaciones</span>
+                                <span className="video-data__time-ago">hace 20 horas</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        Ibai reacciona a Zilverk por primera vez
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name">DonIbair</span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations">34.100 visualizaciones</span>
+                                <span className="video-data__time-ago">hace 20 horas</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/jMrGx7IM7js/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDhCIQb9wSRNnPrRkij5aGk-FLavg" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/oFH4cTGLOfs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCvS1zs6OJxwxu7EU6SHKQlMVvqiw" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li className="list__item">
+                <a className="list__link" href="">
+                    <img className="list__thumbnail" src="https://i.ytimg.com/vi/WUc1bXJE5DY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDjjAE9Rta9zddjsdu7-a7Ycmy3HQ" alt="" />
+                </a>
+                <div className="data">
+                    <img className="data__avatar" src="https://yt3.ggpht.com/oTWD8g9f1uNBmnqBwXDgldmhruLNJpStmPs1SUDBbx6G__3cJBaxK28mChARg36R0gPa9Fc0=s68-c-k-c0x00ffffff-no-rj" alt="" />
+                    <div className="details">
+                        <a className="data__title" href="">
+                        </a>
+                        <div className="metadata">
+                            <span className="metadata__user-name"></span>
+                            <div className="video-data">
+                                <span className="video-data__visualizations"></span>
+                                <span className="video-data__time-ago"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+    </div>
   )
 }
