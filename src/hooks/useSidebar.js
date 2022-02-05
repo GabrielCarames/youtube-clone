25d41ed9e79c977e7aa66bb178fcb10e651ec908
Gabriel Caramés <49535753+GabrielCarames@users.noreply.github.com>
2022-02-05 11:02:53 -0300

    Auth system bug fixed, and access token now is permanent

--- src/hooks/useSidebar.js
+++ src/hooks/useSidebar.js
@@ -4,17 +4,18 @@ import axios from "axios";
 
 export const useSidebar = () => {
     const {setSubscriptions} = useContext(SubscriptionsContext)
+    const accessToken = localStorage.getItem('accessToken')
     
     useEffect(() => {
-        getSuscriptionsFromUserLogged()
+        getSuscriptionsFromUserLogged(accessToken)
     }, []);
     
-    const getSuscriptionsFromUserLogged = async () => {
-        const accessToken = localStorage.getItem('accessToken')
+    const getSuscriptionsFromUserLogged = async (accessToken) => {
         const API_KEY = import.meta.env.VITE_API_KEY
         const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true&maxResults=50&mine=true&order=unread&access_token=${accessToken}&key=${API_KEY}`)
         console.log("suscriptions", res)
         setSubscriptions(res.data.items)
     }
 
+    return {getSuscriptionsFromUserLogged}
 };
